c9aea3e79d290d7efb711e42872add0ada565daa
Pablo Veiga <vcpablo@gmail.com>
2017-06-17 18:16:35 -0300

    Initial commit

---
+++ api/README.md
@@ -0,0 +1,13 @@
+# PHP API CEFET Web Project
+
+> A PHP REST API Project
+
+## Build Setup
+
+``` bash
+# install dependencies
+composer install
+
+# serve with hot reload at localhost:8008
+php -S localhost:8008
+```
---
+++ vuejs2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+      <app-header></app-header>
+      <div class="container">
+          <router-view></router-view>
+      </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+      appHeader: Header
+  }
+}
+</script>
